e4f4d1934495e24807c83712992ec4045d5c0854
ThomasPDye <tompdye@googlemail.com>
2022-07-20 14:49:43 +0100

    attempt2

--- freecad/FCKeyboard/init_gui.py
+++ freecad/FCKeyboard/init_gui.py
@@ -22,7 +22,7 @@ class KeyboardWorkbench(Gui.Workbench):
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        from freecad.FCKeyboard.klepy import keyboard
+        from klepy import keyboard
         App.Console.PrintMessage("switching to FCKeyboard\n")
 
         self.appendToolbar("Tools", self.toolbox)
